e33196c817065bf6f511ac70626a985c59eb40b5
fanchann <fannyarifnass@gmail.com>
2023-06-01 12:23:46 +0700

    change database.go to config

---
+++ pkg/environments/config.go
@@ -0,0 +1,36 @@
+package environments
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"fanchann/library/pkg/utils"
+)
+
+var (
+	APP_PORT string
+	APP_URL  string
+
+	Driver   string
+	Username string
+	Password string
+	Db_name  string
+	Db_url   string
+	Db_port  string
+)
+
+func LoadEnv() {
+	errEnv := godotenv.Load(".env")
+	utils.LogErrorWithPanic(errEnv)
+
+	APP_PORT = os.Getenv("APP_PORT")
+	APP_URL = os.Getenv("APP_URL")
+
+	Driver = os.Getenv("DB_DRIVER")
+	Username = os.Getenv("DB_AUTH_USERNAME")
+	Password = os.Getenv("DB_AUTH_PASSWORD")
+	Db_name = os.Getenv("DB_NAME")
+	Db_url = os.Getenv("DB_URL")
+	Db_port = os.Getenv("DB_PORT")
+}
